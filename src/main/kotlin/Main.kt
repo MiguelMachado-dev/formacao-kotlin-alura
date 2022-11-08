19c434df0aab1e3d9b3d45578638f3e899c74f53
Miguel Machado <miiguellmp@gmail.com>
2022-11-08 14:35:35 -0300

    feat: criando novas classes
    - Criando classe Funcionario

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
     println("Bem vindo ao Bytebank")
+
+    val miguel = Funcionario(
+        nome = "Miguel",
+        cpf = "111.111.111-11",
+        salario = 16000.0
+    )
+
+    println("nome ${miguel.nome}")
+    println("cpf ${miguel.cpf}")
+    println("salario ${miguel.salario}")
+    println("Bonificação ${miguel.bonificacao()}")
 }

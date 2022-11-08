96f7bf5301c8fc0625726d9c1d5a64591d8c7088
Miguel Machado <miiguellmp@gmail.com>
2022-11-08 11:34:24 -0300

    feat: Conhecendo Orientaçao a Objetos
    - Criando classes e objetos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,20 @@
-fun main(){
+fun main() {
     println("Bem vindo ao Bytebank")
-    val titular = "Miguel"
-    val numeroConta = 1000
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+    println("Conta da ${contaAlex.titular} de numero ${contaAlex.numero} com saldo de ${contaAlex.saldo}")
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+    println("Conta da ${contaFran.titular} de numero ${contaFran.numero} com saldo de ${contaFran.saldo}")
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
     var saldo = 0.0
-
-    if (titular.length > 1)
-        saldo = 100.0
-
-    println("Titular $titular")
-    println("numero da conta $numeroConta")
-    println("Saldo da conta $saldo")
 }

20b8671899f6da33ce88822233805d66c332ac3f
Miguel Machado <miiguellmp@gmail.com>
2022-11-08 14:28:30 -0300

    feat: criando novas classes
    - Organizando o projeto

---
+++ src/main/kotlin/Conta.kt
@@ -0,0 +1,32 @@
+class Conta(
+    val titular: String,
+    val numero: Int
+) {
+    var saldo = 0.0
+        private set
+
+    fun deposita(valor: Double) {
+        // this.saldo += valor
+        if (valor > 0) {
+            saldo += valor
+        }
+    }
+
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        } else {
+            println("Valor de $valor indisponível para saque na conta $titular")
+        }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (saldo >= valor) {
+            saldo -= valor
+            destino.deposita(valor)
+            return true
+        }
+
+        return false
+    }
+}
---
+++ src/main/kotlin/testaComportamentosConta.kt
@@ -0,0 +1,20 @@
+fun testaComportamentosConta() {
+    // usando labels
+    val contaAlex = Conta(titular = "Alex", numero = 1000)
+    contaAlex.deposita(200.0)
+    contaAlex.saca(250.0)
+    println("Conta da ${contaAlex.titular} de numero ${contaAlex.numero} com saldo de ${contaAlex.saldo}")
+
+    val contaFran = Conta("Fran", 1001)
+    contaFran.deposita(300.0)
+    println("Conta da ${contaFran.titular} de numero ${contaFran.numero} com saldo de ${contaFran.saldo}")
+
+    if (contaFran.transfere(100.0, contaAlex)) {
+        println("Transferência sucedida")
+    } else {
+        println("Falha na transferência")
+    }
+
+    println("Conta da ${contaAlex.titular} de numero ${contaAlex.numero} com saldo de ${contaAlex.saldo}")
+    println("Conta da ${contaFran.titular} de numero ${contaFran.numero} com saldo de ${contaFran.saldo}")
+}

2a7316f6349605d79f1af4c1436994b640fa721c
Miguel Machado <miiguellmp@gmail.com>
2022-11-08 12:27:18 -0300

    feat: implementando comportamentos
    - Implementando os métodos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,14 @@ fun main() {
     contaAlex.titular = "Alex"
     contaAlex.numero = 1000
     contaAlex.saldo = 200.0
+    contaAlex.saca(250.0)
     println("Conta da ${contaAlex.titular} de numero ${contaAlex.numero} com saldo de ${contaAlex.saldo}")
 
     val contaFran = Conta()
     contaFran.titular = "Fran"
     contaFran.numero = 1001
     contaFran.saldo = 300.0
+    contaFran.deposita(25.1)
     println("Conta da ${contaFran.titular} de numero ${contaFran.numero} com saldo de ${contaFran.saldo}")
 }
 
@@ -17,4 +19,17 @@ class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
+
+    fun deposita(valor: Double) {
+        // this.saldo += valor
+        saldo += valor
+    }
+
+    fun saca(valor: Double) {
+        if (saldo >= valor) {
+            saldo -= valor
+        } else {
+            println("Valor de $valor indisponível para saque na conta $titular")
+        }
+    }
 }

9c321d1877d58723cbbe9839c6bd65b0624e1b6f
Miguel Machado <miiguellmp@gmail.com>
2022-11-08 13:23:07 -0300

    feat: implementando comportamentos
    - Implementando getter e setter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,30 +3,31 @@ fun main() {
     val contaAlex = Conta()
     contaAlex.titular = "Alex"
     contaAlex.numero = 1000
-    contaAlex.saldo = 200.0
+    contaAlex.setSaldo(-200.0)
     contaAlex.saca(250.0)
-    println("Conta da ${contaAlex.titular} de numero ${contaAlex.numero} com saldo de ${contaAlex.saldo}")
+    println("Conta da ${contaAlex.titular} de numero ${contaAlex.numero} com saldo de ${contaAlex.getSaldo()}")
 
     val contaFran = Conta()
     contaFran.titular = "Fran"
     contaFran.numero = 1001
-    contaFran.saldo = 300.0
+    contaFran.setSaldo(300.0)
     contaFran.deposita(25.1)
-    println("Conta da ${contaFran.titular} de numero ${contaFran.numero} com saldo de ${contaFran.saldo}")
+    println("Conta da ${contaFran.titular} de numero ${contaFran.numero} com saldo de ${contaFran.getSaldo()}")
 
     if (contaFran.transfere(100.0, contaAlex)) {
         println("Transferência sucedida")
     } else {
         println("Falha na transferência")
     }
-    println("Conta da ${contaFran.titular} de numero ${contaFran.numero} com saldo de ${contaFran.saldo}")
-    println("Conta da ${contaAlex.titular} de numero ${contaAlex.numero} com saldo de ${contaAlex.saldo}")
+
+    println("Conta da ${contaAlex.titular} de numero ${contaAlex.numero} com saldo de ${contaAlex.getSaldo()}")
+    println("Conta da ${contaFran.titular} de numero ${contaFran.numero} com saldo de ${contaFran.getSaldo()}")
 }
 
 class Conta {
     var titular = ""
     var numero = 0
-    var saldo = 0.0
+    private var saldo = 0.0
 
     fun deposita(valor: Double) {
         // this.saldo += valor
@@ -44,10 +45,20 @@ class Conta {
     fun transfere(valor: Double, destino: Conta): Boolean {
         if (saldo >= valor) {
             saldo -= valor
-            destino.saldo += valor
+            destino.deposita(valor)
             return true
         }
 
         return false
+    }
+
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+    fun setSaldo(valor: Double) {
+        if (valor < 0) return
+
+        saldo = valor
     }
 }

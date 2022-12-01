dba7465f3ad96e1d6b84ab4e8e4138e0fbcb4bac
Miguel Machado <miiguellmp@gmail.com>
2022-12-01 10:10:23 -0300

    feat: alterando elemento do array

--- src/main/kotlin/dev/miguelmachado/bytebank/Main.kt
+++ src/main/kotlin/dev/miguelmachado/bytebank/Main.kt
@@ -20,4 +20,20 @@ fun main() {
     }
 
     println("Menor idade: $menorIdade")
+
+    val salarios: DoubleArray = doubleArrayOf(1500.50, 2300.0, 5000.0, 8000.0, 10000.0)
+    val aumento = 1.1
+
+    for (indice in salarios.indices) {
+        salarios[indice] = salarios[indice] * aumento
+    }
+
+    println(salarios.contentToString())
+
+    // mesma ideia, mas usando forEach
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * aumento
+    }
+
+    println(salarios.contentToString())
 }

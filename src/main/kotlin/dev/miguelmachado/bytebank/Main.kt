5ad30ae97dede40a8b8ff699f45818fd103e2954
Miguel Machado <miiguellmp@gmail.com>
2022-12-01 09:33:11 -0300

    feat: iterando por um array - Kotlin: Desenvolva com coleções, arrays e listas

--- src/main/kotlin/dev/miguelmachado/bytebank/Main.kt
+++ src/main/kotlin/dev/miguelmachado/bytebank/Main.kt
@@ -1,7 +1,23 @@
 package dev.miguelmachado.bytebank
 
-import testaComportamentosConta
-
 fun main() {
-    testaComportamentosConta()
+    val idades: IntArray = intArrayOf(25, 19, 33, 20, 55, 40)
+
+    var maiorIdade = 0
+    for (idade in idades) {
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
+    }
+
+    println("Maior idade: $maiorIdade")
+
+    var menorIdade = Int.MAX_VALUE
+    idades.forEach {
+        if (it < menorIdade) {
+            menorIdade = it
+        }
+    }
+
+    println("Menor idade: $menorIdade")
 }

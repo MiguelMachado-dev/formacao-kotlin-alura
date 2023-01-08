05b9e17bbbb5d42c60c35fb7dcf11bafd4c15878
Miguel Machado <miguel.machado@cornershopapp.com>
2023-01-08 15:38:02 -0300

    feat: gasto total com fold

--- colecoes-recursos-do-funcional/src/main/kotlin/SalariosComBigDecimal.kt
+++ colecoes-recursos-do-funcional/src/main/kotlin/SalariosComBigDecimal.kt
@@ -16,6 +16,13 @@ fun main() {
 
     val gastoInicial = salariosComAumento.somatoria()
     println(gastoInicial)
+
+    val meses = "6".toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial) { acumulador, salario ->
+        acumulador + (salario * meses).setScale(2, RoundingMode.UP)
+    }
+
+    println(gastoTotal)
 }
 
 private fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal): BigDecimal =

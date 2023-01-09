ff0995edb72a4e3f83705ec6ca78ac530b2c345d
Miguel Machado <miguel.machado@cornershopapp.com>
2023-01-08 21:50:16 -0300

    feat: listas imutáveis

--- colecoes-recursos-do-funcional/src/main/kotlin/dev/miguelmachado/list/Prateleira.kt
+++ colecoes-recursos-do-funcional/src/main/kotlin/dev/miguelmachado/list/Prateleira.kt
@@ -2,15 +2,13 @@ package dev.miguelmachado.list
 
 data class Prateleira(
     val genero: String,
-    val livros: MutableList<Livro>
+    val livros: List<Livro>
 ) {
-    fun organizarPorAutor(): MutableList<Livro> {
-        livros.sortBy { it.autor }
-        return livros
+    fun organizarPorAutor(): List<Livro> {
+        return livros.sortedBy { it.autor }
     }
 
-    fun organizarPorAnoPublicacao(): MutableList<Livro> {
-        livros.sortBy { it.anoPublicacao }
-        return livros
+    fun organizarPorAnoPublicacao(): List<Livro> {
+        return livros.sortedBy { it.anoPublicacao }
     }
 }

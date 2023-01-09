254ec4f5633c0167e51c1f88b1089e705bdf9630
Miguel Machado <miguel.machado@cornershopapp.com>
2023-01-08 21:08:55 -0300

    feat: ordenando listas

--- colecoes-recursos-do-funcional/src/main/kotlin/dev/miguelmachado/list/ListaDeLivros.kt
+++ colecoes-recursos-do-funcional/src/main/kotlin/dev/miguelmachado/list/ListaDeLivros.kt
@@ -39,9 +39,14 @@ fun main() {
     )
 
     livros.imprimeComMarcadores()
+
+    val ordenadoAnoPublicacao: List<Livro> = livros.sorted()
+    ordenadoAnoPublicacao.imprimeComMarcadores()
+
+    livros.sortedBy { it.titulo }.imprimeComMarcadores()
 }
 
-fun MutableList<Livro>.imprimeComMarcadores() {
+fun List<Livro>.imprimeComMarcadores() {
     val textoFormatado = this.joinToString(separator = "\n") {
         " - ${it.titulo} de ${it.autor}"
     }
--- colecoes-recursos-do-funcional/src/main/kotlin/dev/miguelmachado/list/Livro.kt
+++ colecoes-recursos-do-funcional/src/main/kotlin/dev/miguelmachado/list/Livro.kt
@@ -5,4 +5,8 @@ data class Livro(
     val autor: String,
     val anoPublicacao: Long,
     val editora: String? = null
-)
+) : Comparable<Livro> {
+    override fun compareTo(other: Livro): Int {
+        return this.anoPublicacao.compareTo(other.anoPublicacao)
+    }
+}

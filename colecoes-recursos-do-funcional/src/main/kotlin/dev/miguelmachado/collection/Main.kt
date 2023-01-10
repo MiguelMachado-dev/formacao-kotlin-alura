7aa58b5384a892ef9dd976af5280b140001198f8
Miguel Machado <miguel.machado@cornershopapp.com>
2023-01-10 17:29:06 -0300

    feat: introducao a colecoes

---
+++ colecoes-recursos-do-funcional/src/main/kotlin/dev/miguelmachado/collection/Main.kt
@@ -0,0 +1,11 @@
+package dev.miguelmachado.collection
+
+fun main() {
+    val nomes: List<String> = listOf(
+        "Alex",
+        "Fran",
+        "Gui",
+        "Maria"
+    )
+    println(nomes)
+}
